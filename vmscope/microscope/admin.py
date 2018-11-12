3bdf3f60be619b7c676d1f342a584153c51140ab
Likit Preeyanon <likit.pre@mahidol.edu>
2018-11-12 14:29:16 +0700

    added artifact image models

--- vmscope/microscope/admin.py
+++ vmscope/microscope/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Parasite, ParasiteImage, ParasiteStage
+from .models import *
 
 # Register your models here.
 admin.site.register(ParasiteImage)
 admin.site.register(Parasite)
 admin.site.register(ParasiteStage)
+admin.site.register(ArtifactImage)
+admin.site.register(ArtifactType)
+admin.site.register(Artifact)
--- vmscope/microscope/models.py
+++ vmscope/microscope/models.py
@@ -3,6 +3,49 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+class ArtifactType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Artifact(models.Model):
+    group = models.ForeignKey('ArtifactType', related_name='artifacts',
+                              on_delete=models.CASCADE)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{} {}'.format(self.group, self.desc)
+
+
+class ArtifactImage(models.Model):
+    artifact = models.ForeignKey(Artifact, related_name='images',
+                                 on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=200, choices=(
+        ('pending', 'pending'),
+        ('approved', 'approved'),
+        ('disabled', 'disabled')
+    ), default='pending')
+    upload_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    upload_by = models.ForeignKey('auth.User', related_name='artifact_images',
+                                  on_delete=models.CASCADE)
+
+    def image_name(self, filename):
+        extension = os.path.splitext(filename)[-1]
+        return '{}/{}'.format(self.artifact.group.name,
+                   datetime.now().strftime('%Y%m%d_%H%M%S'), extension)
+
+    image = models.ImageField(max_length=200,
+                              upload_to=image_name)
+
+    def __str__(self):
+        return '{}'.format(self.image.url)
+
+
+
 class Parasite(models.Model):
     genus = models.CharField(max_length=200)
     species = models.CharField(max_length=200)
