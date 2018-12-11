312bc23c284de818fe18d2f238b273fd966c9342
Likit Preeyanon <likit.pre@mahidol.edu>
2018-12-11 09:19:38 +0700

    added parasite report page
    updated the microscope page

--- vmscope/microscope/urls.py
+++ vmscope/microscope/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ParasiteImageListView, MicroscopeView
+from .views import ParasiteImageListView, MicroscopeView, parasite_report
 
 
 urlpatterns = [
     path('section/<int:pk>', MicroscopeView.as_view(), name='microscope'),
-    path('images/', ParasiteImageListView.as_view(), name='image_list')
+    path('images/', ParasiteImageListView.as_view(), name='image_list'),
+    path('parasite_report/<int:pk>', parasite_report, name='parasite_report')
 ]
--- vmscope/microscope/views.py
+++ vmscope/microscope/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import ParasiteImage, MicroscopeSection
+from .models import ParasiteImage, MicroscopeSection, Parasite
 
 # Create your views here.
 class ParasiteImageListView(ListView):
     model = ParasiteImage
     template_name = 'microscope/pr_image_list.html'
     context_object_name = 'images'
+
+
+def parasite_report(request, pk):
+    corrects = []
+    incorrects = []
+    scope_section = get_object_or_404(MicroscopeSection, pk=int(pk))
+    parasites = Parasite.objects.all()
+    if request.method == 'POST':
+        answers = set(request.POST.getlist('answers'))
+        parasite_components = []
+        for item in scope_section.parasite_components.all():
+            if str(item.parasite.id) in answers:
+                corrects.append(str(item.parasite))
+            else:
+                incorrects.append(str(item.parasite))
+    return render(request, 'microscope/parasite_report.html',
+                  {'parasites': parasites,
+                   'scope_section': scope_section,
+                   'corrects': corrects,
+                   'incorrects': incorrects,
+                   })
 
 
 class MicroscopeView(DetailView):
