581768d4ce45e23bc6e0ade3fe7ad2cda059c002
Likit Preeyanon <likit.pre@mahidol.edu>
2019-12-10 02:30:35 +0900

    added key page for a tagset
    added back button on the session page

--- vmscope/photohunt/urls.py
+++ vmscope/photohunt/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (ImageListView, ImageView,
                     DisciplinePageView, ProgramPageView,
-                    show_session, show_tagset,)
+                    show_session, show_tagset, show_tagset_key)
 
 app_name = "photohunt"
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('discipline/<int:pk>', DisciplinePageView.as_view(), name='show_discipline'),
     path('program/<int:pk>', ProgramPageView.as_view(), name='show_program'),
     path('session/<int:pk>', show_session, name='show_session'),
+    path('session/<int:session_pk>/tagset/<int:tsnum>/key', show_tagset_key, name='show_tagset_key'),
     path('session/<int:session_pk>/tagset/<int:tsnum>', show_tagset, name='show_tagset'),
     path('image_view/<int:pk>', ImageView.as_view(), name='image_view'),
     path('images/', ImageListView.as_view(), name='image_list'),
--- vmscope/photohunt/views.py
+++ vmscope/photohunt/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import ListView, DetailView
-from .models import Image, Session, Discipline, Program
+from .models import Image, Session, Discipline, Program, ImageTagSet
 
 
 class DisciplinePageView(DetailView):
@@ -65,7 +65,9 @@ def show_tagset(request, session_pk, tsnum=0):
         return render(request,
                       'photohunt/session_end.html',
                       {'results': tagsets_results,
-                       'session': session})
+                       'session': session,
+                       'tsnum': tsnum,
+                       })
 
     try:
         tagset = tagsets[tsnum]
@@ -83,3 +85,26 @@ def show_tagset(request, session_pk, tsnum=0):
                    'tagset_number': tsnum + 1,
                    'tsnum': tsnum,
                    })
+
+
+def show_tagset_key(request, session_pk, tsnum):
+    session = get_object_or_404(Session, pk=session_pk)
+    tagsets = list(session.tagsets.all())
+    if tsnum >= len(tagsets):
+        return redirect(reverse('photohunt:show_session',
+                                kwargs={'pk': session.pk}))
+
+    tagset = tagsets[tsnum]
+
+    all_tags = []
+    for n,tag in enumerate(tagset.tags.all(), start=1):
+        all_tags.append({'x': tag.x, 'y': tag.y, 'n': n})
+    return render(request,
+                  'photohunt/tagset_key.html',
+                  {'session': session,
+                   'tagset': tagset,
+                   'all_tags': all_tags,
+                   'tagset_number': tsnum + 1,
+                   'tsnum': tsnum,
+                   })
+
